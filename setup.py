965762ab122b47df3bf427ca6d082ec2811a728c
The-Spine <dtorkelson@gmail.com>
2021-10-29 15:02:27 -0400

    add package requirements

--- setup.py
+++ setup.py
@@ -4,5 +4,8 @@ from setuptools import setup, find_packages
 setup(
     name='billreader',
     version='0.1.0',
-    packages=find_packages(include=['billreader'])
+    packages=find_packages(include=['billreader']),
+    install_requires=[
+        'pdfminer.six>=20211012',
+    ]
 )
