4d64546359a91be0201145498b56c1a5f48f908b
dtork@dolores <dtorkelson@gmail.com>
2022-01-24 19:57:23 +0000

    add version to docker image builder

--- image_builder.sh
+++ image_builder.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Builds the docker image, passing the git hash as a build arg
+# and the billreader package version as the tag.
+# 
+# Run this from the root of the package.
 
-cd ~/Documents/Python/bill-pdfs/
+tag_version="$(python3 setup.py --version)"
 export GIT_HASH=$(git rev-parse HEAD)
-docker build --build-arg GIT_HASH=${GIT_HASH::7} -t billreader .
+docker build --build-arg GIT_HASH=${GIT_HASH::7} -t "billreader:$tag_version" .
